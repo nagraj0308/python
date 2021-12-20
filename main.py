8f8558bb08a6aae2d91f92ba9f46e46bcaaca4b4
nagraj0308 <nagendrachaudhary2016@gmail.com>
2021-12-20 17:59:29 +0530

    restructure

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from program1 import basics_of_python
-from program2 import all_loops
-from program3 import all_list
-from program4 import all_tuple
+from basic_programs.program1 import basics_of_python
+from basic_programs.program2 import all_loops
+from basic_programs.program3 import all_list
+from basic_programs.program4 import all_tuple
 
 # basics_of_python()
 # all_loops()
 # all_list()
-all_tuple()
+# all_tuple()
