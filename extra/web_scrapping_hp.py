3dcfdfbcb08b2fc60ba57ffeb5041218ff34d32d
nagraj0308 <nagendrachaudhary2016@gmail.com>
2022-08-24 06:57:14 +0530

    fc

--- extra/web_scrapping_hp.py
+++ extra/web_scrapping_hp.py
@@ -1,38 +1,69 @@
-from bs4 import BeautifulSoup
-import requests
 import pandas as pd
-import re
-
-URL = "https://opentender.eu/"
-# URL = "https://sam.gov/content/home"
-# URL = "https://www.fdot.gov/procurement/advertisements.shtm"
-# URL = "https://apps3.txdot.gov/apps-cq/project_tracker/"
-# URL = "https://dot.ca.gov/programs/procurement-and-contracts/contracts-out-for-bid"
-# URL = "https://www.gov.uk/contracts-finder"
-p = '[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+'
-
-r = requests.get(URL)
-print(r.content)
-soup = BeautifulSoup(r.content,
-                     'html5lib')  # If this line causes an error, run 'pip install html5lib' or install html5lib
-
-data = [["ProductId", "ProductName", "Price"]]
-table = soup.find('div', attrs={'class': 'AH_ProductListInner d-flex removeSpanDiv'})
+import requests
+from bs4 import BeautifulSoup
+from word2number import w2n
 
 
-def get_price(ss):
-    s = str(ss)
-    if re.search(p, s) is not None:
-        for catch in re.finditer(p, s):
-            return catch[0]
+# First website
+def web_scrap(url):
+    site = requests.get(url)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    table = soup.find('table')
+    # print(table)
+    headers = []
+    for i in table.find_all('th'):
+        title = i.text.strip()
+        headers.append(title)
+        # print(title)
+    mydata = pd.DataFrame(columns=headers)
+
+    for j in table.find_all('tr')[1:]:
+        row_data = j.find_all('td')
+        row = [i.text.strip() for i in row_data]
+        length = len(mydata)
+        mydata.loc[length] = row
+
+    return mydata
 
 
-for row in table.findAll('div',
-                         attrs={'class': 'AH_ProductView col-lg-3 col-md-3 col-sm-6 col-xs-6 productThumbnails'}):
-    product_id = row.find('div', attrs={'class': 'yotpo bottomLine'}).get('data-product-id')
-    product_name = row.find('a', attrs={'class': 'prFeatureName'}).text
-    price = get_price(row.find('span', attrs={'class': 'rs'}))
-    data.append([product_id, product_name, price])
+url = 'https://dot.ca.gov/programs/procurement-and-contracts/contracts-out-for-bid'
+df1 = web_scrap(url)
+df1.to_csv('file1.csv')
 
-f = pd.DataFrame(data)
-f.to_csv("WebScrapingIB.csv", index=False, header=False)
+
+# second site(https://opentender.eu/all/search/tender) with huge data
+def web_scrap_new(url):
+    site = requests.get(url)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    table = soup.find('table')
+    # print(table)
+    headers = []
+    for i in table.find_all('th'):
+        title = i.text.strip()
+        headers.append(title)
+        # print(title)
+    mydata = pd.DataFrame(columns=headers)
+
+    num = soup.select("pagination div")[1].text.split("Entries:")[1].strip().split(" ")
+
+    pages = w2n.word_to_num(num[1])
+    pages = int(pages * float(num[0]))
+    pages = 100
+    for p in range(0, pages):
+        soup = BeautifulSoup(requests.get(url.format(p)).content)
+        table = soup.find('table')
+
+        for j in table.find_all('tr')[1:]:
+            row_data = j.find_all('td')
+            row = [i.text.strip() for i in row_data]
+            length = len(mydata)
+            # print(length)
+            mydata.loc[length] = row
+            print(row)
+        print(p, "/", pages-1)
+
+    return mydata
+
+
+df2 = web_scrap_new('https://opentender.eu/all/search/tender')
+df2.to_csv('file2.csv')
