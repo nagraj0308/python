72ac53641d9f82088b67fd80100a02905e333bd6
nagraj0308 <nagendrachaudhary2016@gmail.com>
2023-09-15 18:51:04 +0530

    crlt +l

--- extra/multilingual.py
+++ extra/multilingual.py
@@ -1,5 +1,23 @@
 import pandas as pd
+# Specify the path to your XML file
+fli = "strings.xml"
+flo = "strings.csv"
+i = 0
+j = 0
+datao=[]
 
-df = pd.read_csv('data.csv')
 
-print(df) 
+with open(fli, 'r') as file:
+    for line in file:
+        i = i + 1
+        l = line.strip()
+        item_id = l.split('"')[1]
+        t1 = l.split('"')[2].split("</string>")[0][1:]
+        print(item_id, t1)
+        j= j + len(t1.split(" "))
+        datao.append([item_id, t1])
+    print(i,j)
+    print(datao)
+    f = pd.DataFrame(datao)
+    f.to_csv(flo, index=False)
+
