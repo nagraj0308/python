93a8eca1e9353e0e6b32d520d9ee12fa16edb66e
nagraj0308 <nagendrachaudhary2016@gmail.com>
2023-11-08 10:34:59 +0530

    has

--- extra/multilingual1.py
+++ extra/multilingual1.py
@@ -1,11 +1,11 @@
 import pandas as pd
+
 # Specify the path to your XML file
 fli = "strings.xml"
 flo = "strings.csv"
 i = 0
 j = 0
-datao=[]
-
+datao = []
 
 with open(fli, 'r') as file:
     for line in file:
@@ -14,10 +14,9 @@ with open(fli, 'r') as file:
         item_id = l.split('"')[1]
         t1 = l.split('"')[2].split("</string>")[0][1:]
         print(item_id, t1)
-        j= j + len(t1.split(" "))
+        j = j + len(t1.split(" "))
         datao.append([item_id, t1])
-    print(i,j)
+    print(i, j)
     print(datao)
     f = pd.DataFrame(datao)
     f.to_csv(flo, index=False)
-
---
+++ extra/multilingual2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Specify the path to your XML file
+fli = "strings_pun.csv"
+flo = "strings_pun.xml"
+i = 0
+
+# <string name="d8">8</string>
+with open(fli, 'r') as file:
+    for line in file:
+        i = i + 1
+        l = line.strip()
+        items = l.split(',')
+        str1 = "<string name=\"" + items[0] + "\">" + items[2] + "</string>"
+        print(str1)
+    print(i)
